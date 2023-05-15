8ed72a736dd92754b1d8005238ba0dc1a5aead70
Krishna murari pandey <80660583+pmurarikrishna@users.noreply.github.com>
2023-05-15 22:35:43 +0530

    Add files via upload

---
+++ usingrecursion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main 
+ {
+    int num1, num2;
+    cout<<"enter first number" <<endl ;
+    cin>> num1 ;
+    cout << "enter second number "<<endl ;
+    cin>>>num2>>endl ;
+    cout<<"the  sum of two no is "<< num1+num2 ;
+    return 0 ;
+}
